a49c1436e4db4bf51adf8a6f693acd772ac06be4
B-Bischoff <mainbischoffbrice@gmail.com>
2023-01-20 09:24:33 -0500

    rofi + graphic programming packets

--- Debian11_install/packets_install.sh
+++ Debian11_install/packets_install.sh
@@ -12,4 +12,22 @@ i3 \
 compton \
 nitrogen \
 python3-venv \
+ripgrep \
 cmake \
+rofi \
+
+# graphic programming packets
+sudo -y \
+apt-get \
+install \
+libsoil-dev \
+libglm-dev \
+libglew-dev \
+libglfw3-dev \
+libxinerama-dev \
+libxcursor-dev \
+libxi-dev \
+libfreetype-dev \
+libgl1-mesa-dev \
+xorg-dev  \
+freeglut3-dev
