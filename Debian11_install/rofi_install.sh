648e263def08080eb0fb88d8035de3890a1c8e64
B-Bischoff <mainbischoffbrice@gmail.com>
2023-01-23 08:49:53 -0500

    added missing packages

--- Debian11_install/rofi_install.sh
+++ Debian11_install/rofi_install.sh
@@ -7,6 +7,7 @@ libgdk-pixbuf2.0-dev \
 libstartup-notification0-dev \
 libxkbcommon-dev \
 libglib2.0-dev \
+libxcb-xinerama0-dev \
 libxcb-randr0-dev \
 libxcb-cursor-dev \
 libxkbcommon-x11-dev \
@@ -16,7 +17,8 @@ libxcb-ewmh-dev \
 libxcb-icccm4-dev \
 libxcb-util-dev \
 libpangocairo-1.0-0 \
-libpango1.0-dev
+libpango1.0-dev \
+check
 
 echo ""
 echo "-----------------------------------------------------------------"
